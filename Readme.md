01d9a11ecdbf524bc734de7a6a086b79ca56499d
Sergio <36644639+sergiogar99@users.noreply.github.com>
2021-02-12 18:00:37 +0100

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,2 @@
+#  Validar si esta bien construida la aplicacion
+#  rasa validate data
---
+++ actions/actions.py
@@ -0,0 +1,16 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+from rasa_sdk.interfaces import Action
+
+class ActionGreet(Action):
+    def name(self):
+        return 'action_greet'
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message(template="utter_greet")
+        return []
